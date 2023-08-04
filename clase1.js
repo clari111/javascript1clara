de4aee2f5ed39dc9c538e5acd7efb17415a9e83c
alumno <alumno@Instituto.local>
2023-08-04 08:38:35 -0300

    n

---
+++ clase1.js
@@ -0,0 +1,14 @@
+console.log ( 20 + 40 ) 
+console.log  ( 50 - 30 )
+console.log ( 30 * 7 )
+console.log  (70 / 4 )
+let numerosSuma = 20 + 40 
+let numeroResta = 50 - 30
+let numeroMultiplicacion = 30 * 7
+let numeroDivision = 70 / 4
+
+alert(numerosSuma)
+alert(numeroMultiplicacion)
+alert(numeroDivision)
+alert(numeroResta)
+
