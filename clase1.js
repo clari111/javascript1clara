e3063f5d083eb7e2f5ef09c2cbd63de283d56def
alumno <alumno@Instituto.local>
2023-08-11 08:12:39 -0300

    terminado

--- clase1.js
+++ clase1.js
@@ -14,6 +14,23 @@ alert('el resultado de la resta es ' + (numeroResta))
 
 alert('mi edad es:' + (2023-2009))
 
+let edadusuario
+edadusuario = prompt('Ingresa tu edad')
+alert ('tu edad es ' + edadusuario)
+
 let nombreAlumno
 nombreAlumno = prompt('Ingresa tu nombre')
 alert('Bienvenido ' + nombreAlumno)
+
+let nombreAlumnoyo = ('Clara')
+let apellidoAlumnoyo = (' Gauna')
+
+alert('Bienvenido ' + nombreAlumnoyo + apellidoAlumnoyo)
+
+console.log ('Bienvenida Clara Gauna')
+
+let Diez = 10
+let Tres = 3
+let resultadoMultiplicacion = 10 / 3
+
+alert (resultadoMultiplicacion)
